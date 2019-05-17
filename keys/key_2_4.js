20824c8e63c7efff411aa6b29df3d993e809a60f
FLYFAN <oncwnuHv6d0vSjvoRm6Eps864IiY@git.weixin.qq.com>
2019-05-17 22:29:56 +0800

    fix key_2_4

--- keys/key_2_4.js
+++ keys/key_2_4.js
@@ -1,95 +1,42 @@
-window.key_2_1 = {
-  'key-3-2-7': {
-      type: 'blank',
-      content: `1. peculiar   2. radical  3. phase   4. sensible 5.  predict
- 6. labeled  7. resent  8.  witnessed 9.  equivalent  10.parallels`
-  },
-
-
-
-  'key-3-2-9': {
-
-      type: 'blank',
-
-      content: 'chaotic/dramatic/academy/depression/detection/erosion/classification/confuse/cooperate/dictate/rightist/journal'
-
-  },
-
-  'key-3-2-10': {
-
-      type: 'blank',
-
-      content: ` 1.journal  2.chaotic  3.cooperate  4.erosion  5.dramatic   6. confuse     
-
-7.academy  8.rightists  9.depression  10.dictate 11.detection  12.classification
-
-
-  `
-
-  },
-
-  'key-3-2-11': {
-
-      type: 'cloze',
-
-      content: `1-5: K E A C L         6-10:  I G N H O`
-
-  },
-
-  'key-3-2-12': {
-
-      type: 'blank',
-
-      content: `1. saddled with  2. back off  3.gives way to   4.resorted to  5.make allowances for  6. wonder at 7. prior to  8.based upon/on `
-
-  },
-
-  'key-3-6-4': {
-
-      type: 'mc',
-
-      content: `1.A  2. C   3. D   4. A   5. B   6. D   7. D    8. C  `
-
-  },
-
-  'key-3-6-6': {
-
-      type: 'blank',
-
-      content: `1.hollow   2. restless  3.demonstrate   4.exiled   5.miserable 
-
-6.commercial  7. hesitated  8.erupt  9.refine 10.feasible`
-
-  },
-
-  'key-3-6-7': {
-
-      type: 'blank',
-
-      content: `1. are tired of  2.fall into  3.be strict with  4.not amount to mach  
-
-5.drifted off  6.bonded with  7.resign himself to  8.surrender themselves to   
-
-
-  `
-
-  },
-
-  'key-3-7-2': {
-
-      type: 'collocation',
-
-      content: `1.journey   2.pressure   3.generation   4.attitude    5.challenge    6.need    7.change    8.aspirations`
-
-  },
-
-  'key-3-7-3': {
-
-      type: 'blankB',
-
-      content: `1.fluid  2.contemporary   3.radical  4.enormous   5.prideful  6.competitive  7.rebellious   
-8.transition  9.unavoidable   10. traditional  11.unique   12.peculiar `
-
-  }
-
+window.key_2_4 = {
+    'key-4-2-7': {
+        type: 'blank',
+        content: '1.confess 2.tempting 3.commence 4.coordinating 5.granted 6.deserved 7.displayed 8.consequently 9.lodged 10.proceed'
+    },
+    'key-4-2-9': {
+        type: 'blank',
+        content: 'contented/compress/complicate/identification/qualification/illustration/exhibit/extension/interpretation/plantation/perception'
+    },
+    'key-4-2-10': {
+        type: 'blank',
+        content: '1.qualifications 2.perception 3.plantations 4.compressed 5.exhibit 6.contented 7.interpretation 8.complicate 9.illustration 10.extension 11.identification 12.justification'
+    },
+    'key-4-2-11': {
+        type: 'cloze',
+        content: '1-5: N B G I H        6-10: L E M K C'
+    },
+    'key-4-2-12': {
+        type: 'blank',
+        content: '1.on her behalf 2.took the liberty of 3.immune to 4.were filled with 5.fall in love with 6.Expelled from 7.been pessimistic about 8.go along with'
+    },
+    'key-4-6-4': {
+        type: 'mc',
+        content: '1.D  2.C  3.A  4.C  5.A  6.D  7.B  8.C'
+    },
+    'key-4-6-6': {
+        type: 'blank',
+        content: '1.conveying 2.forged 3.thriving 4.abnormal 5.frowning 6.thrilled 7.reckoned 8.clarified 9.conquer 10.concealing'
+    },
+    'key-4-6-7': {
+        type: 'blank',
+        content: '1.contact with 2.took the initiative 3.are inclined to 4.thrive on 5.indifferent to 6.whip out 7.in the pit of her stomach 8.chip in'
+    },
+    'key-4-7-2': {
+        type: 'collocation',
+        content: '1.casual attraction 2.took the initiative 3.romantic relationship 4.dynamic personality 5.mature love 6.mutual friend'
+    },
+    'key-4-7-3': {
+        type: 'blankB',
+        content: '1.college sweetheart 2.take the initiative 3.romantic relationship 4.steady boyfriend 5.casual attraction 6.fairy-tale romance 7.exceedingly embarrassed 8.madly in love 9.healthy friendship 10.traditional upbringing 11.mature love 12.mutual friends'
+    }
 }
