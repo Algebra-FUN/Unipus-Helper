be8c3a2c29ed16a2f7c9da26b08183a000b32a98
GodFatherZhang <50621120+GodFatherZhang@users.noreply.github.com>
2019-10-12 22:21:32 +0800

    Update key_3_1.js

--- keys/key_3_1.js
+++ keys/key_3_1.js
@@ -2,56 +2,45 @@ window.key_3_1 = {
   'key-139-1-2-7': {
       type: 'blank',
       content: `1.whereby   2.pursuit   3.inhibit    4.maintain   5.patriotic
-  6.transcend   7.endeavor    8.dedication   9.prestige   10.nominate
+  6.transcended   7.endeavors    8.dedication   9.prestige   10.nominate
   `
   },
   'key-139-1-2-9': {
       type: 'blank',
-      content: 'inhabitant/participant/attendant/pollutant/descendant/contestant/tolerant/neglectful/resultant/resourceful/boastful/respectful'
+      content: 'inhabit/participant/attendant/pollutant/descendant/contestant/tolerant/resultful/neglectful/resourceful/boastful/respectful'
   },
   'key-139-1-2-10': {
       type: 'blank',
-      content: `1.resultant   2.   tolerant   3.pollutants   4.inhabited   5.participants  
-  6.descendants   7.attendants   8.respectful   9.contestants   10.neglectful   11.resourceful   12.boastful
+      content: `1.resultant   2.   tolerant   3.pollutants   4.inhabited   5.contestants  
+  6.descendants   7.attendants   8.respectful   9.participants   10.neglectful   11.resourceful   12.boastful
   `
   },
   'key-139-1-2-11': {
       type: 'cloze',
-      content: `1-5:  L C J A I          6-10:  O N E H F`
+      content: `1-5:  F G H J E          6-10:  A N I K M`
   },
   'key-139-1-2-12': {
       type: 'blank',
-      content: `1.removed from  2.failed in  3.in pursuit of  4.deviated from  5.precluded from   6.triumph over   7.work their way into  8.written off`
+      content: `removed from  2.failed in  3.in the pursuit of  4.deviated from  5.precludes   6.from   7.triumph over   8.work their way into   9.written off`
   },
   'key-139-1-6-4': {
       type: 'mc',
-      content: `C   2. B   3. A   4. D   5. B   6. B   7. C    8. D  `
+      content: `C   2. B   3. A   4. D   5. D   6. B   7. C    8. D  `
   },
   'key-139-1-6-6': {
       type: 'blank',
-      content: `indulge   2. propelled   3.  aggravated    4. dazzled   5.  alleviated
-  6. renowned    7. eloquent     8. destined     9. scorns    10 Applause
-  `
+      content: `1.indulge   2. propelling   3.  aggravated    4. dazzled   5.  alleviating  6. renowned    7. eloquent     8. destined     9. scorned    10.applause `
   },
   'key-139-1-6-7': {
       type: 'blank',
-      content: `up     2. in      3.  on         4. up  
-  5.  to   6. on   7. as     8. out 
-  `
+      content: `up     2. in  3.on  4. up  5.  to   6. on   7. as   8. out `
   },
   'key-139-1-7-2':{
-      type:'collocation',
-      content:`essential framework     2. distinctive difference      3 high standards  
-      4 valuable possessions     5 considerable frustration      6 acquire knowledge    
-      7 overcome deficiencies    8 sketch thoughts             9 devise a scheme
-     `
+      type:'blank',
+      content:`repeated     2 overwhelming      3 immense  4 heroic     5 sound      6 substantial    7 attained    8 fueled     9 achieve`
   },
   'key-139-1-7-3': {
       type: 'blankB',
-      content: `1 sudden opportunities    2 immense obstacles   3 amazing determination  4 profound difficulties
-  5 overwhelming failures        6 poverty-stricken  7 substantial hardship 
-   8  repeated misfortunes    9 sheer persistence       10 dazzle audiences 
-   11 achieve fame     12 strong will
-  `
+      content: `1 sudden opportunity    2 immense obstacles   3 amazing determination  4 profound difficulties  5 overwhelming failures        6 poverty   6 -stricken   8 substantial hardships 9  repeated misfortunes    10 sheer persistence       11 dazzle audience 12 achieve fame     13 strong will `
   }
 }
