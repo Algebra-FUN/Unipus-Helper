f7520f9fbd1a37d3a4406f3adb1e6d535cfc8aa6
GodFatherZhang <50621120+GodFatherZhang@users.noreply.github.com>
2019-10-12 22:32:16 +0800

    Create key_3_7.js

---
+++ keys/key_3_7.js
@@ -0,0 +1,44 @@
+window.key_3_7 = {
+    'key-139-7-2-7': {
+        type: `blank`,
+        content: `1.donate 2.spiral 3.termination 4.layoff 5.subsistence 6.spectrum 7.complied 8.reclaimed 9.originated 10.expired`
+    },
+    'key-139-7-2-9': {
+        type: `blank`,
+        content: `emission/omission/suspension/prediction/distribution/corrupt/generosity/formality/locality/maturity/peculiarity/liability`
+    },
+    'key-139-7-2-10': {
+        type: `blank`,
+        content: `1.emission 2.suspension 3.formalities 4.prediction 5.omissions 6.distribution 7.locality 8.peculiarity 9.corrupting 10.generosity 11.maturity 12.liability`
+    },
+    'key-139-7-2-11': {
+        type: `cloze`,
+        content: ` 1-5:  E J B H A       6-10:  K G I C N`
+    },
+    'key-139-7-2-12': {
+        type: `blank`,
+        content: `1.put down   2.wind up   3.scrape together  4.on the verge of  5.in all likelihood  6.deteriorate  into 7.are in a position  8.gave way to`
+    },
+    'key-139-7-6-4': {
+        type: `mc`,
+        content: `1.A  2.A  3.C  4.C  5.D  6.B   7.A    8.D`
+},
+    'key-139-7-6-6': {
+        type: `blank`,
+        content: `1.amplifying   2.assimilate  3.intrinsic  4.entail   5.commodity   6.mentality  7.envisage  8.allocated  9.equity  10.ambiguous`
+    },
+    'key-139-7-6-7': {
+        type: `blank`,
+        content: `1.in   2.on   3.in  4.in  5.into  6.off  7.in  8.to`
+    },
+    'key-139-7-7-2': {
+        type: `cloze`,
+        content: `1. e  2. 3. a  4. d  5. f   6. c`
+	},
+    'key-139-7-7-3':
+        {
+            type: `blankB`,
+            content:
+                `1.falling sharply    2.unimaginable situation    3.economic slump  4.tenant eviction   5.purchasing power     6.further increases 7.stock market crash    8.sinking finances     9.negative equity    10.daily stress     11.long-term unemployment 12.employment spectrum`
+        }
+}
