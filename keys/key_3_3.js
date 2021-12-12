af931d4b6118fd748fb032fd194a8daa7c834167
Pushinl <pushinliu@gmail.com>
2021-12-12 17:57:03 +0800

    Book3题库add

---
+++ keys/key_3_3.js
@@ -0,0 +1,46 @@
+window.key_3_3 = {
+    'key-139-3-2-7': {
+        type: `blank`,
+        content: `1.integral 2.cherish 3.afflicted 4.noteworthy 5.portray 6.compliment 7.domain 8.anonymous 9.conscientious 10.perpetual`
+    },
+    'key-139-3-2-9': {
+        type: `blank`,
+        content: `generalize/normalize/publicize/minimize/immunize/mobilize/investor/dictator/conqueror/investigator/moderator/elevate`
+    },
+    'key-139-3-2-10': {
+        type: `blank`,
+        content: `1.normalize 2.moderator 3.immunized 4.investors 5.mobilize 6.conqueror 7.elevate 8.publicizes 9.investigator 10.minimized 11.generalize 12.dictator`
+    },
+    'key-139-3-2-11': {
+        type: `cloze`,
+        content: ` 1-5:  C I F L A      6-10:  H K N E B`
+    },
+    'key-139-3-2-12': {
+        type: `blank`,
+        content: `1.embark on 2.be deprived of 3.turn down 4.taken captive 5.live on 6.share in 7.was stricken by 8.led by example`
+    },
+    'key-139-3-5-2': {
+        type: `mc`,
+        content: `1.A 2.B   3.A   4.B   5.B  6.A  7.A   8.B`
+    },
+    'key-139-3-6-4': {
+        type: `mc`,
+        content: `1.D  2.C  3.D  4.A  5.B  6.C  7.D  8.D`
+    },
+    'key-139-3-6-6': {
+        type: `blank`,
+        content: `1.skeptical 2.coincidence 3.mounted 4.contrive 5.simulated  6.manifest  7.divert  8.infected  9.upgraded  10.temperament`
+    },
+    'key-139-3-6-7': {
+        type: `blank`,
+        content: `1.on 2.in 3.as 4.into  5.to  6.to 7.with 8.under`
+    },
+    'key-139-3-7-2': {
+        type: `collocation`,
+        content: `1.i famous     2.h schedule        3.f hope          4.a draining 5.d dangerous   6.b misery           7.c signify            8.g interest       9.e try`
+    },
+    'key-139-3-7-3': {
+        type: `blank`,
+        content: `1.internationally 2.committed 3.compassionate 4.repeatedly 5.thrill 6.terrific 7.overwhelming 8.desperate 9.physically 10.rectified 11.personal 12.emotionally`
+    }
+}
---
+++ keys/key_3_6.js
@@ -0,0 +1,42 @@
+window.key_3_6 = {
+    "key-139-6-2-7": {
+        type: "blank",
+        content: `1.evacuated 2.stray 3.diluted 4.gigantic 5.standpoint
+    6.nutrition 7.inflicted 8.pervasive 9.naive 10.permeated
+    `,
+    },
+    "key-139-6-2-10": {
+        type: "blank",
+        content: `1.visionary   2.fatality   3.availability   4.customary   5.feasibility   6.momentary   7.disciplinary
+        8.supplementary   9.missionary   10.authenticity   11.complexity   12.desirability`,
+    },
+    "key-139-6-2-11": {
+        type: "cloze",
+        content: `1-5:  H N O F C          6-10:  K M B E J`,
+    },
+    "key-139-6-2-12": {
+        type: "blank",
+        content: `1.flew at   2.wove her way through   3.radiated from    4.conceive of   5.was lined with
+        6.dive into   7.next to nothing  8.stayed down`,
+    },
+    "key-139-6-6-4": {
+        type: "mc",
+        content: `1.B 2.D 3.D 4.C 5.A 6.C 7.B 8.D`,
+    },
+    "key-139-6-6-6": {
+        type: "blank",
+        content: `1.supervise   2.petitioned   3.clutching   4.vicinity   5.fragrance   6.dispatched   7.dwelling   8.haunted   9.timid   10.encloses`,
+    },
+    "key-139-6-6-7": {
+        type: "blank",
+        content: `1.to   2.in   3.together   4.apart   5.with   6.off   7.with   8.off`,
+    },
+    "key-139-6-7-2": {
+        type: "mc",
+        content: `1.A 2.C 3.B 4.B 5.C 6.B`,
+    },
+    "key-139-6-7-3": {
+        type: "blankB",
+        content: `1.emotional intensity   2.brilliant sky   3.walked briskly   4.helplessly wandering   5.trembling voice   6.tremendous roar   7.deserted road   8.violently dipping   9.inevitable death   10.profound fatigue   11.pervasive threat   12.dangerous evil`,
+    },
+};
